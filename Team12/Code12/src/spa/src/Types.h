4f605fbcd7c0b7817bf395f4f92ce3d1602e908c
Alden <49450743+alcen@users.noreply.github.com>
2020-08-30 17:09:12 +0800

    Implementation of Lexer library

--- Team12/Code12/src/spa/src/Types.h
+++ Team12/Code12/src/spa/src/Types.h
@@ -5,10 +5,11 @@
 #ifndef SPA_TYPES_H
 #define SPA_TYPES_H
 
+#include <memory>
 #include <string>
 #include <vector>
 
 typedef std::string String;
-typedef std::vector<std::string> StringList;
+typedef std::vector<std::unique_ptr<std::string>> StringList;
 
 #endif // SPA_TYPES_H
--- Team12/Code12/src/spa/src/lexer/Lexer.h
+++ Team12/Code12/src/spa/src/lexer/Lexer.h
@@ -7,7 +7,7 @@
 
 #include "Types.h"
 
-StringList splitByWhitespace(String programFragment);
-StringList splitByDelimiter(String str, String delimiter);
+StringList* splitByWhitespace(const String& programFragment) noexcept;
+StringList* splitByDelimiter(const String& str, const String& delimiter);
 
 #endif // SPA_LEXER_H
