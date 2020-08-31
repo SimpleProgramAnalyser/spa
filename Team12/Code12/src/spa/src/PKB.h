4ca46ae00a72b05e4d5acdebd8c438a74feaa82e
Alden <49450743+alcen@users.noreply.github.com>
2020-08-31 18:08:01 +0800

    Fix .clang-format to adhere to isoCpp style

--- Team12/Code12/src/spa/src/PKB.h
+++ Team12/Code12/src/spa/src/PKB.h
@@ -13,7 +13,7 @@ class TNode;
 class VarTable; // no need to #include "VarTable.h" as all I need is pointer
 
 class PKB {
-    public:
+public:
     static VarTable* varTable;
     static int setProcToAST(PROC p, TNode* r);
     static TNode* getRootAST(PROC p);
--- Team12/Code12/src/spa/src/TNode.h
+++ Team12/Code12/src/spa/src/TNode.h
@@ -1,3 +1,3 @@
 class TNode {
-    public:
+public:
 };
