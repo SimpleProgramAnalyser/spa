946bd1e4296bc4e8842e31bef282123cf1062a7d
Alden <49450743+alcen@users.noreply.github.com>
2020-09-01 19:24:58 +0800

    Complete list of AST nodes

--- Team12/Code12/src/spa/src/ast/AstTypes.cpp
+++ Team12/Code12/src/spa/src/ast/AstTypes.cpp
@@ -7,6 +7,11 @@
 
 #include <string>
 
+ProcedureNode::~ProcedureNode()
+{
+    delete statementList;
+}
+
 String Constant::toString()
 {
     return std::to_string(static_cast<int>(value));
@@ -15,4 +20,147 @@ String Constant::toString()
 String Variable::toString()
 {
     return static_cast<String>(varName);
+}
+
+StatementType ReadStatementNode::getStatementType()
+{
+    return ReadStatement;
+}
+
+StatementNumber ReadStatementNode::getStatementNumber()
+{
+    return stmtNum;
+}
+
+StatementType PrintStatementNode::getStatementType()
+{
+    return PrintStatement;
+}
+
+StatementNumber PrintStatementNode::getStatementNumber()
+{
+    return stmtNum;
+}
+
+StatementType CallStatementNode::getStatementType()
+{
+    return CallStatement;
+}
+
+StatementNumber CallStatementNode::getStatementNumber()
+{
+    return stmtNum;
+}
+
+NotExpression::~NotExpression()
+{
+    delete expression;
+}
+
+ConditionalExpressionType NotExpression::getConditionalType() noexcept
+{
+    return NotConditionalExpression;
+}
+
+AndExpression::~AndExpression()
+{
+    delete leftExpression;
+    delete rightExpression;
+}
+
+ConditionalExpressionType AndExpression::getConditionalType() noexcept
+{
+    return AndConditionalExpression;
+}
+
+OrExpression::~OrExpression()
+{
+    delete leftExpression;
+    delete rightExpression;
+}
+
+ConditionalExpressionType OrExpression::getConditionalType() noexcept
+{
+    return OrConditionalExpression;
+}
+
+ArithmeticExpression::ArithmeticExpression(Expression* const left, Expression* const right, ExpressionOperator op):
+    leftFactor(left), rightFactor(right), op(op)
+{}
+
+ArithmeticExpression::~ArithmeticExpression()
+{
+    delete leftFactor;
+    delete rightFactor;
+}
+
+bool ArithmeticExpression::isArithmetic() noexcept
+{
+    return true;
+}
+
+ReferenceExpression::~ReferenceExpression()
+{
+    delete basicData;
+}
+
+bool ReferenceExpression::isArithmetic() noexcept
+{
+    return false;
+}
+
+ConditionalExpressionType RelationalExpression::getConditionalType() noexcept
+{
+    return RelationalConditionalExpression;
+}
+
+RelationalExpression::~RelationalExpression()
+{
+    delete leftFactor;
+    delete rightFactor;
+}
+
+IfStatementNode::~IfStatementNode()
+{
+    delete ifStatementList;
+    delete elseStatementList;
+}
+
+StatementType IfStatementNode::getStatementType()
+{
+    return IfStatement;
+}
+
+StatementNumber IfStatementNode::getStatementNumber()
+{
+    return stmtNum;
+}
+
+WhileStatementNode::~WhileStatementNode()
+{
+    delete statementList;
+}
+
+StatementType WhileStatementNode::getStatementType()
+{
+    return WhileStatement;
+}
+
+StatementNumber WhileStatementNode::getStatementNumber()
+{
+    return stmtNum;
+}
+
+AssignmentStatementNode::~AssignmentStatementNode() {
+    delete expression;
+}
+
+StatementType AssignmentStatementNode::getStatementType()
+{
+    return AssignmentStatement;
+}
+
+StatementNumber AssignmentStatementNode::getStatementNumber()
+{
+    return stmtNum;
 }
--- Team12/Code12/src/spa/src/ast/AstTypes.h
+++ Team12/Code12/src/spa/src/ast/AstTypes.h
@@ -9,6 +9,7 @@
 #include <Types.h>
 
 typedef String Name;
+typedef Integer StatementNumber;
 
 enum StatementType {
     AnyStatement = 0,
@@ -24,7 +25,7 @@ class StatementNode {
 public:
     virtual ~StatementNode() = 0;
     virtual StatementType getStatementType() = 0;
-    virtual Integer getStatementNumber() = 0;
+    virtual StatementNumber getStatementNumber() = 0;
 };
 
 class StatementListNode {
@@ -34,13 +35,14 @@ public:
 
 class ProcedureNode {
 public:
-    Name procName;
-    StatementListNode statementList;
+    const Name procedureName;
+    const StatementListNode* const statementList;
+    ~ProcedureNode();
 };
 
 class ProgramNode {
 public:
-    Name progName;
+    const Name programName;
     List<ProcedureNode> procedureList;
 };
 
@@ -52,14 +54,168 @@ public:
 
 class Constant: public BasicDataType {
 public:
-    Integer value;
+    const Integer value;
     String toString() override;
+    ~Constant() override = default;
 };
 
 class Variable: public BasicDataType {
 public:
-    Name varName;
+    const Name varName;
     String toString() override;
+    ~Variable() override = default;
+};
+
+class ReadStatementNode: public StatementNode {
+public:
+    const Variable var;
+    StatementType getStatementType() override;
+    StatementNumber getStatementNumber() override;
+
+private:
+    StatementNumber stmtNum;
+};
+
+class PrintStatementNode: public StatementNode {
+public:
+    const Variable var;
+    StatementType getStatementType() override;
+    StatementNumber getStatementNumber() override;
+
+private:
+    StatementNumber stmtNum;
+};
+
+class CallStatementNode: public StatementNode {
+public:
+    const Name procedureName;
+    StatementType getStatementType() override;
+    StatementNumber getStatementNumber() override;
+
+private:
+    StatementNumber stmtNum;
+};
+
+enum ConditionalExpressionType {
+    RelationalConditionalExpression = 0,
+    NotConditionalExpression = 1,
+    AndConditionalExpression = 2,
+    OrConditionalExpression = 4
+};
+
+class ConditionalExpression {
+public:
+    virtual ~ConditionalExpression() = 0;
+    virtual ConditionalExpressionType getConditionalType() noexcept = 0;
+};
+
+class NotExpression: public ConditionalExpression {
+public:
+    ~NotExpression() override;
+    const ConditionalExpression* expression;
+    ConditionalExpressionType getConditionalType() noexcept override;
+};
+
+class AndExpression: public ConditionalExpression {
+public:
+    ~AndExpression() override;
+    const ConditionalExpression* const leftExpression;
+    const ConditionalExpression* const rightExpression;
+    ConditionalExpressionType getConditionalType() noexcept override;
+};
+
+class OrExpression: public ConditionalExpression {
+public:
+    ~OrExpression() override;
+    const ConditionalExpression* const leftExpression;
+    const ConditionalExpression* const rightExpression;
+    ConditionalExpressionType getConditionalType() noexcept override;
+};
+
+enum ExpressionOperator : char {
+    PlusOperator = '+',
+    MinusOperator = '-',
+    TimesOperator = '*',
+    DivideOperator = '/',
+    ModuloOperator = '%'
+};
+
+class Expression {
+public:
+    virtual ~Expression() = 0;
+    virtual bool isArithmetic() noexcept = 0;
+};
+
+class ArithmeticExpression: public Expression {
+public:
+    const Expression* const leftFactor;
+    const Expression* const rightFactor;
+    const ExpressionOperator opr;
+    ArithmeticExpression(Expression* left, Expression* right, ExpressionOperator op);
+    ~ArithmeticExpression() override;
+    bool isArithmetic() noexcept override;
+};
+
+class ReferenceExpression: public Expression {
+public:
+    const BasicDataType* const basicData;
+    ~ReferenceExpression() override;
+    bool isArithmetic() noexcept override;
+};
+
+enum RelationalOperator : char {
+    GreaterThanOperator = '>',
+    GreaterThanOrEqualToOperator = '}',
+    LessThanOperator = '<',
+    LessThanOrEqualToOperator = '{',
+    NotEqualToOperator = '!',
+    EqualToOperator = '='
+};
+
+class RelationalExpression: public ConditionalExpression {
+public:
+    const Expression* const leftFactor;
+    const Expression* const rightFactor;
+    const RelationalOperator opr;
+    ~RelationalExpression() override;
+    ConditionalExpressionType getConditionalType() noexcept override;
+};
+
+class IfStatementNode: public StatementNode {
+public:
+    const ConditionalExpression* const predicate;
+    const StatementListNode* const ifStatementList;
+    const StatementListNode* const elseStatementList;
+    ~IfStatementNode() override;
+    StatementType getStatementType() override;
+    StatementNumber getStatementNumber() override;
+
+private:
+    StatementNumber stmtNum;
+};
+
+class WhileStatementNode: public StatementNode {
+public:
+    const ConditionalExpression* const predicate;
+    const StatementListNode* const statementList;
+    ~WhileStatementNode() override;
+    StatementType getStatementType() override;
+    StatementNumber getStatementNumber() override;
+
+private:
+    StatementNumber stmtNum;
+};
+
+class AssignmentStatementNode: public StatementNode {
+public:
+    const Variable variable;
+    const Expression* expression;
+    ~AssignmentStatementNode() override;
+    StatementType getStatementType() override;
+    StatementNumber getStatementNumber() override;
+
+private:
+    StatementNumber stmtNum;
 };
 
 #endif // SPA_AST_TYPES_H
