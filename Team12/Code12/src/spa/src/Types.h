36cff7494eb2138d106140c43a4295d90fced60a
Chester Sim <chestersimdy@gmail.com>
2020-09-05 11:05:14 +0800

    NOT WORKING Add AqTypes

--- Team12/Code12/src/spa/src/Types.h
+++ Team12/Code12/src/spa/src/Types.h
@@ -8,8 +8,13 @@
 #include <memory>
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 typedef std::string String;
 typedef std::vector<std::unique_ptr<std::string>> StringList;
+
+// PQL
+
+
 
 #endif // SPA_TYPES_H
---
+++ Team12/Code12/src/spa/src/pql/Evaluator.h
@@ -0,0 +1,12 @@
+//
+// Created by Chester Sim on 3/9/20.
+//
+
+#ifndef AUTOTESTER_EVALUATOR_H
+#define AUTOTESTER_EVALUATOR_H
+
+#include <Types.h>
+
+String* evaluateQuery(AbstractQuery abstractQuery);
+
+#endif // AUTOTESTER_EVALUATOR_H
