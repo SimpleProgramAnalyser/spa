b5bc432eba571fa9d1a34eeead6ac9f83b1a003b
Alden <49450743+alcen@users.noreply.github.com>
2020-09-07 21:29:55 +0800

    Unit tests for Lexer component

--- Team12/Code12/src/spa/src/lexer/Lexer.h
+++ Team12/Code12/src/spa/src/lexer/Lexer.h
@@ -10,5 +10,6 @@
 StringList* splitByWhitespace(const String& programFragment) noexcept;
 StringList* splitByDelimiter(const String& str, const String& delimiter);
 StringList* splitProgram(const String& program) noexcept;
+String trimWhitespace(const String& str);
 
 #endif // SPA_LEXER_H
