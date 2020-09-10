8b31362d8353970ff13fac1ca6a93d65be267b80
Alden <49450743+alcen@users.noreply.github.com>
2020-09-10 23:55:31 +0800

    Add test case for complicated parseExpression

--- Team12/Code12/src/unit_testing/src/Parser_Test.cpp
+++ Team12/Code12/src/unit_testing/src/Parser_Test.cpp
@@ -1,7 +1,8 @@
 #include "catch.hpp"
 #include "frontend/parser/Parser.cpp"
 
-std::string getDefaultProgramName() {
+std::string getDefaultProgramName()
+{
     return "SIMPLE program";
 }
 
@@ -40,35 +41,22 @@ procedure compute {\n\
     REQUIRE(*(ast) == *(constructedAst));
 }
 
-TEST_CASE("Parser parses if statements correctly") {
+TEST_CASE("Parser parses if statements correctly") {}
 
-}
+TEST_CASE("Parser parses while statements correctly") {}
 
-TEST_CASE("Parser parses while statements correctly") {
+TEST_CASE("Parser parses complicated arithmetic expressions statements correctly") {}
 
-}
+TEST_CASE("Parser parses complicated conditional expressions statements correctly") {}
 
-TEST_CASE("Parser parses complicated arithmetic expressions statements correctly") {
+TEST_CASE("Parser parses multiple procedures correctly") {}
 
-}
+TEST_CASE("Parser parses keywords as identifiers correctly") {}
 
-TEST_CASE("Parser parses complicated conditional expressions statements correctly") {
+TEST_CASE("Parser parses variables with same name as procedure") {}
 
-}
-
-TEST_CASE("Parser parses multiple procedures correctly") {
-
-}
-
-TEST_CASE("Parser parses keywords as identifiers correctly") {
-
-}
-
-TEST_CASE("Parser parses variables with same name as procedure") {
-
-}
-
-TEST_CASE("parseArithmeticExpression works for plus expression") {
+TEST_CASE("parseExpression works for plus expression")
+{
     auto* arithmeticExpression = new StringList(3);
     String x = "x";
     String plus = "+";
@@ -81,10 +69,109 @@ TEST_CASE("parseArithmeticExpression works for plus expression") {
     REQUIRE(*(parsed) == *(expected));
 }
 
-TEST_CASE("Syntax error, mismatched braces") {
+TEST_CASE("parseExpression obeys operator precedence and brackets")
+{
+    //         >=====< bracket1
+    //         |     |   >====================< bracket2
+    //     >=========< timesExpression1       |
+    //     |   :     |   |    >=======< timesExpression2
+    //     |   :     |   |    |       |       |
+    // x + y * (z / a) - (b + c / d * e - f + g)
+    String str0 = "x";
+    String str1 = "+";
+    String str2 = "y";
+    String str3 = "*";
+    String str4 = "(";
+    String str5 = "z";
+    String str6 = "/";
+    String str7 = "a";
+    String str8 = ")";
+    String str9 = "-";
+    String str10 = "(";
+    String str11 = "b";
+    String str12 = "+";
+    String str13 = "c";
+    String str14 = "/";
+    String str15 = "d";
+    String str16 = "*";
+    String str17 = "e";
+    String str18 = "-";
+    String str19 = "f";
+    String str20 = "+";
+    String str21 = "g";
+    String str22 = ")";
+    auto* arithmeticExpression = new StringList(23);
+    arithmeticExpression->at(0) = std::unique_ptr<std::string>(&str0);
+    arithmeticExpression->at(1) = std::unique_ptr<std::string>(&str1);
+    arithmeticExpression->at(2) = std::unique_ptr<std::string>(&str2);
+    arithmeticExpression->at(3) = std::unique_ptr<std::string>(&str3);
+    arithmeticExpression->at(4) = std::unique_ptr<std::string>(&str4);
+    arithmeticExpression->at(5) = std::unique_ptr<std::string>(&str5);
+    arithmeticExpression->at(6) = std::unique_ptr<std::string>(&str6);
+    arithmeticExpression->at(7) = std::unique_ptr<std::string>(&str7);
+    arithmeticExpression->at(8) = std::unique_ptr<std::string>(&str8);
+    arithmeticExpression->at(9) = std::unique_ptr<std::string>(&str9);
+    arithmeticExpression->at(10) = std::unique_ptr<std::string>(&str10);
+    arithmeticExpression->at(11) = std::unique_ptr<std::string>(&str11);
+    arithmeticExpression->at(12) = std::unique_ptr<std::string>(&str12);
+    arithmeticExpression->at(13) = std::unique_ptr<std::string>(&str13);
+    arithmeticExpression->at(14) = std::unique_ptr<std::string>(&str14);
+    arithmeticExpression->at(15) = std::unique_ptr<std::string>(&str15);
+    arithmeticExpression->at(16) = std::unique_ptr<std::string>(&str16);
+    arithmeticExpression->at(17) = std::unique_ptr<std::string>(&str17);
+    arithmeticExpression->at(18) = std::unique_ptr<std::string>(&str18);
+    arithmeticExpression->at(19) = std::unique_ptr<std::string>(&str19);
+    arithmeticExpression->at(20) = std::unique_ptr<std::string>(&str20);
+    arithmeticExpression->at(21) = std::unique_ptr<std::string>(&str21);
+    arithmeticExpression->at(22) = std::unique_ptr<std::string>(&str22);
+    Expression* parsed = parseExpression(arithmeticExpression);
+    Expression* bracket1 = createDivExpr(createRefExpr("z"), createRefExpr("a"));
+    Expression* timesExpression1 = createTimesExpr(createRefExpr("y"), bracket1);
 
+    Expression* timesExpression2
+        = createTimesExpr(createDivExpr(createRefExpr("c"), createRefExpr("d")), createRefExpr("e"));
+    Expression* bracket2 = createPlusExpr(
+        createMinusExpr(createPlusExpr(createRefExpr("b"), timesExpression2), createRefExpr("f")), createRefExpr("g"));
+
+    Expression* expected = createMinusExpr(createPlusExpr(createRefExpr("x"), timesExpression1), bracket2);
+    REQUIRE(*(parsed) == *(expected));
 }
 
-TEST_CASE("Syntax error, mismatched brackets") {
-
+TEST_CASE("parseExpression does not work for while statement")
+{
+    String str0 = "while";
+    String str1 = "(";
+    String str2 = "x";
+    String str3 = "==";
+    String str4 = "y";
+    String str5 = ")";
+    String str6 = "{";
+    String str7 = "x";
+    String str8 = "=";
+    String str9 = "x";
+    String str10 = "-";
+    String str11 = "1";
+    String str12 = ";";
+    String str13 = "}";
+    auto* arithmeticExpression = new StringList(14);
+    arithmeticExpression->at(0) = std::unique_ptr<std::string>(&str0);
+    arithmeticExpression->at(1) = std::unique_ptr<std::string>(&str1);
+    arithmeticExpression->at(2) = std::unique_ptr<std::string>(&str2);
+    arithmeticExpression->at(3) = std::unique_ptr<std::string>(&str3);
+    arithmeticExpression->at(4) = std::unique_ptr<std::string>(&str4);
+    arithmeticExpression->at(5) = std::unique_ptr<std::string>(&str5);
+    arithmeticExpression->at(6) = std::unique_ptr<std::string>(&str6);
+    arithmeticExpression->at(7) = std::unique_ptr<std::string>(&str7);
+    arithmeticExpression->at(8) = std::unique_ptr<std::string>(&str8);
+    arithmeticExpression->at(9) = std::unique_ptr<std::string>(&str9);
+    arithmeticExpression->at(10) = std::unique_ptr<std::string>(&str10);
+    arithmeticExpression->at(11) = std::unique_ptr<std::string>(&str11);
+    arithmeticExpression->at(12) = std::unique_ptr<std::string>(&str12);
+    arithmeticExpression->at(13) = std::unique_ptr<std::string>(&str13);
+    Expression* parsed = parseExpression(arithmeticExpression);
+    REQUIRE(parsed == nullptr);
 }
+
+TEST_CASE("Syntax error, mismatched braces") {}
+
+TEST_CASE("Syntax error, mismatched brackets") {}
