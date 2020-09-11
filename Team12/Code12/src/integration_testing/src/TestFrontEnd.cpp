6ee33628e81279e9949672877edc89964706b541
Nathanael Seen <e0345677@u.nus.edu>
2020-09-11 15:51:42 +0800

    Removed unncessary files from project

--- Team12/Code12/src/integration_testing/src/TestFrontEnd.cpp
+++ Team12/Code12/src/integration_testing/src/TestFrontEnd.cpp
@@ -1,13 +1,14 @@
-#include "TNode.h"
 #include "catch.hpp"
 using namespace std;
+// #include "TNode.h"
+
 void require(bool b) {
     REQUIRE(b);
 }
 
 TEST_CASE("1st Test") {
 
-    TNode T;
-    require(1 == 1);
+    /*TNode T;
+    require(1 == 1);*/
 }
 
