22b459a1c35a01b55fd3c907387eb529033ecacd
Tan Yee Jian <tanyeejian@gmail.com>
2020-09-12 23:08:32 +0800

    PKB: Update Follows implementation, add unit tests

--- Team12/Code12/src/spa/src/pkb/relationships/Follows.cpp
+++ Team12/Code12/src/spa/src/pkb/relationships/Follows.cpp
@@ -4,55 +4,66 @@
 
 #include "Follows.h"
 
+void FollowsTable::tryAddBefore(Integer before, StatementType beforeType, StatementType afterType,
+                                ArrayArrayList& aal, ArrayArraySet& aas)
+{
+    // store the before statement into (lists of statements before type afterStmtType)
+    auto setBeforeType = &aas[afterType].byType[beforeType];
+    // returns true if new object is created (emplace is successful)
+    if (setBeforeType->find(before) == setBeforeType->end()) {
+        setBeforeType->insert(before);
+        aal[afterType].byType[beforeType].push_back(before);
+    }
+}
+void FollowsTable::tryAddAfter(Integer after, StatementType beforeType, StatementType afterType,
+                               ArrayArrayList& aal, ArrayArraySet& aas)
+{
+    // store the after statement into (lists of statements after type afterStmtType)
+    auto setAfterType = &aas[beforeType].byType[afterType];
+    // returns true if new object is created (emplace is successful)
+    if (setAfterType->find(after) == setAfterType->end()) {
+        setAfterType->insert(after);
+        aal[beforeType].byType[afterType].push_back(after);
+    }
+}
 // important private helper function
 void FollowsTable::typedShenanigans(Integer before, StatementType beforeStmtType, Integer after,
                                     StatementType afterStmtType)
 {
-    // store the before statement into (lists of statements before type afterStmtType)
-    auto setBeforeType = &stmtBeforeTypeSet[afterStmtType].byType[beforeStmtType];
-    // returns true if new object is created (emplace is successful)
-    if (setBeforeType->emplace(before).second) {
-        stmtBeforeType[afterStmtType].byType[beforeStmtType].push_back(before);
-    }
+    tryAddBefore(before, beforeStmtType, afterStmtType, stmtBeforeType, stmtBeforeTypeSet);
+    tryAddBefore(before, beforeStmtType, AnyStatement, stmtBeforeType, stmtBeforeTypeSet);
+    tryAddBefore(before, AnyStatement, AnyStatement, stmtBeforeType, stmtBeforeTypeSet);
 
-    // store the after statement into (lists of statements after type beforeStmtType)
-    auto setAfterType = &stmtAfterTypeSet[beforeStmtType].byType[afterStmtType];
-    // returns true if new object is created (emplace is successful)
-    if (setAfterType->emplace(after).second) {
-        stmtAfterType[beforeStmtType].byType[afterStmtType].push_back(after);
-    }
+    tryAddAfter(after, afterStmtType, afterStmtType, stmtAfterType, stmtAfterTypeSet);
+    tryAddAfter(after, afterStmtType, AnyStatement, stmtAfterType, stmtAfterTypeSet);
+    tryAddAfter(after, AnyStatement, AnyStatement, stmtAfterType, stmtAfterTypeSet);
 }
 // important private helper function
 void FollowsTable::typedShenanigansStar(Integer before, StatementType beforeStmtType, Integer after,
                                         StatementType afterStmtType)
 {
-    // store the before statement into (lists of statements before type afterStmtType)
-    auto setBeforeType = &stmtBeforeStarTypeSet[afterStmtType].byType[beforeStmtType];
-    // returns true if new object is created (emplace is successful)
-    if (setBeforeType->emplace(before).second) {
-        stmtBeforeType[afterStmtType].byType[beforeStmtType].push_back(before);
-    }
+    tryAddBefore(before, beforeStmtType, afterStmtType, stmtBeforeStarType, stmtBeforeStarTypeSet);
+    tryAddBefore(before, beforeStmtType, AnyStatement, stmtBeforeStarType, stmtBeforeStarTypeSet);
+    tryAddBefore(before, AnyStatement, AnyStatement, stmtBeforeStarType, stmtBeforeStarTypeSet);
 
-    // store the after statement into (lists of statements after type beforeStmtType)
-    auto setAfterType = &stmtAfterStarTypeSet[beforeStmtType].byType[afterStmtType];
-    // returns true if new object is created (emplace is successful)
-    if (setAfterType->emplace(after).second) {
-        stmtAfterType[beforeStmtType].byType[afterStmtType].push_back(after);
-    }
+    tryAddAfter(after, afterStmtType, afterStmtType, stmtAfterStarType, stmtAfterStarTypeSet);
+    tryAddAfter(after, afterStmtType, AnyStatement, stmtAfterStarType, stmtAfterStarTypeSet);
+    tryAddAfter(after, AnyStatement, AnyStatement, stmtAfterStarType, stmtAfterStarTypeSet);
 }
+
 void FollowsTable::addFollowsRelationships(Integer before, StatementType beforeStmtType, Integer after,
                                            StatementType afterStmtType)
 {
     // store statements before it
-    stmtBeforeMap.insert(after, before);
+    stmtBeforeMap.insert(std::make_pair(after, before));
     // store statements after it
-    stmtAfterMap.insert(before, after);
+    stmtAfterMap.insert(std::make_pair(before, after));
 
     typedShenanigans(before, beforeStmtType, after, afterStmtType);
 }
 
 void FollowsTable::addFollowsRelationshipsStar(Integer before, StatementType beforeStmtType,
-                                               Vector<Pair<Integer, StatementType>> afterStmttypePairs)
+                                               const Vector<Pair<Integer, StatementType>>& afterStmttypePairs)
 {
     for (auto stmtTypePair : afterStmttypePairs) {
         Integer after = stmtTypePair.first;
@@ -60,9 +71,11 @@ void FollowsTable::addFollowsRelationshipsStar(Integer before, StatementType bef
         // set and list
         stmtBeforestarsetMap[after].insert(before);
         stmtBeforestarlistMap[after].byType[beforeStmtType].push_back(before);
+        stmtBeforestarlistMap[after].byType[AnyStatement].push_back(before);
 
         stmtAfterstarsetMap[before].insert(after);
         stmtAfterstarlistMap[before].byType[afterStmtType].push_back(after);
+        stmtAfterstarlistMap[before].byType[AnyStatement].push_back(after);
 
         typedShenanigansStar(before, beforeStmtType, after, afterStmtType);
     }
