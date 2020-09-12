a828356670209af91e0ab1874fd6765e79f16346
Alden <49450743+alcen@users.noreply.github.com>
2020-09-12 20:52:44 +0800

    Fix TestWrapper.cpp not compiling

--- Team12/Code12/src/autotester/src/TestWrapper.cpp
+++ Team12/Code12/src/autotester/src/TestWrapper.cpp
@@ -1,35 +1,42 @@
 #include "TestWrapper.h"
-#include "AbstractWrapper.h"
-#include "FrontendManager.h"
-#include "pql/projector/FormattedQueryResult.h"
-#include "pql/projector/QueryResultFormatType.h"
-#include "pql/PqlManager.h"
 
 #include <iostream>
 
+#include "AbstractWrapper.h"
+#include "frontend/FrontendManager.h"
+#include "pql/PqlManager.h"
+#include "pql/projector/FormattedQueryResult.h"
+#include "pql/projector/QueryResultFormatType.h"
+
 // implementation code of WrapperFactory - do NOT modify the next 5 lines
 AbstractWrapper* WrapperFactory::wrapper = 0;
-AbstractWrapper* WrapperFactory::createWrapper() {
-  if (wrapper == 0) wrapper = new TestWrapper;
-  return wrapper;
+AbstractWrapper* WrapperFactory::createWrapper()
+{
+    if (wrapper == 0)
+        wrapper = new TestWrapper;
+    return wrapper;
 }
 // Do not modify the following line
 volatile bool AbstractWrapper::GlobalStop = false;
 
 // a default constructor
-TestWrapper::TestWrapper() {
-  // create any objects here as instance variables of this class
-  // as well as any initialization required for your spa program
+TestWrapper::TestWrapper()
+{
+    // create any objects here as instance variables of this class
+    // as well as any initialization required for your spa program
 }
 
 // method for parsing the SIMPLE source
 void TestWrapper::parse(std::string filename)
 {
     // need to read from file
+    String program = "procedure TEMP { x = 1; }";
+    parseSimple(program);
 }
 
 // method to evaluating a query
-void TestWrapper::evaluate(std::string query, std::list<std::string>& results){
+void TestWrapper::evaluate(std::string query, std::list<std::string>& results)
+{
     // call your evaluator to evaluate the query here
     // ...code to evaluate query...
 
