965a7ad969e78075e694dde94a32270d50dc400a
Alden <49450743+alcen@users.noreply.github.com>
2020-09-17 00:14:01 +0800

    Populate PatternMatcher with helper methods

--- Team12/Code12/src/spa/src/pql/evaluator/PatternMatcher.cpp
+++ Team12/Code12/src/spa/src/pql/evaluator/PatternMatcher.cpp
@@ -5,11 +5,60 @@
  */
 #include "PatternMatcher.h"
 
+#include "pkb/PKB.h"
+
+/**
+ * Checks whether the clauseExpression can be found
+ * within the programExpression. If it is not found,
+ * check the subtrees of the programExpression
+ * by calling doExpressionsMatch recursively.
+ *
+ * @param programExpression The tree of an Expression from
+ *                          the SIMPLE program, to match the
+ *                          clauseExpression to.
+ * @param clauseExpression The tree of an Expression from a
+ *                         query clause, to match with a
+ *                         subtree in programExpression.
+ * @return True, if clauseExpression matches some subtree
+ *         within programExpression. Otherwise, false.
+ */
+Boolean doExpressionsMatch(Expression* programExpression, Expression* clauseExpression) {}
+
+/**
+ * An enum that represents certain states that the
+ * PatternMatcher can run in, to determine what
+ * results should be returned by the PatternMatcher.
+ *
+ * The enum corresponds to the relationship between
+ * the "Select"ed synonym of the query and the pattern
+ * clause of the query. Specifically, whether the synonym
+ * appears in the pattern clause, and the type of synonym.
+ */
+enum class SynonymAndClauseRelationship { Unrelated, Statement, Variable };
+
+std::vector<String> matchAssignStatement(AssignmentStatementNode* assign, PatternClause* pnClause,
+                                         SynonymAndClauseRelationship relationship)
+{}
+
+std::vector<String> findAssignInStatementList(StmtlstNode* stmtLstNode, PatternClause* pnClause,
+                                              SynonymAndClauseRelationship relationship)
+{}
+
+std::vector<String> evaluateAssignPattern(const Synonym& synonym, PatternClause* pnClause,
+                                          const DeclarationTable& declarations)
+{
+    std::vector<String> result;
+    ProgramNode* ast = getRootNode();
+
+    return result;
+}
+
 std::vector<String> evaluatePattern(const Synonym& synonym, PatternClause* pnClause,
                                     const DeclarationTable& declarations)
 {
-
-    std::vector<String> result;
-
-    return result;
+    if (pnClause->getStatementType() == AssignPatternType) {
+        return evaluateAssignPattern(synonym, pnClause, declarations);
+    } else {
+        throw std::runtime_error("Unknown PatternClause type in evaluatePattern");
+    }
 }
