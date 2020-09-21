46117b7102121ce659abd270cfc97905d3f4b92f
Alden <49450743+alcen@users.noreply.github.com>
2020-09-21 15:50:58 +0800

    Fix testing script, add unit test for Evaluator

--- Team12/Code12/scripts/run-tests.sh
+++ Team12/Code12/scripts/run-tests.sh
@@ -1,34 +1,102 @@
 #!/usr/bin/env bash
 set -euo pipefail
-# Usage: run with --build to build
+# usage: run with number of processors, or argument no-build to skip building
 
 rootdir=$(git rev-parse --show-toplevel)
 builddir="${rootdir}/Team12/Code12/build"
 testdir="${rootdir}/Team12/Tests12"
-BUILD_TYPE="${BUILD_TYPE:-DEBUG}"
+BUILD_TYPE="${BUILD_TYPE:-RELEASE}"
 
-# build if --build is stated
-if [[ $# -gt 0 ]] && [[ $1 == "--build" ]]; then
-    cd ${rootdir}
-    cd Team12/Code12/
-    rm -f -r build
-    mkdir -p build
-    cd build
-    cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
-    cmake --build . -j$(nproc) --config $BUILD_TYPE
+echo_usage() {
+  echo "Usage: ./run-tests.sh <build-or-not> [skip-tests]"
+  echo "Specify build to compile SPA, or no-build to skip build. e.g. ./run-tests.sh build"
+  echo "Specify skip-tests to skip unit tests and integration tests e.g. ./run-tests.sh build skip-tests"
+}
+
+build_all() {
+  cd "${rootdir}"
+  cd Team12/Code12/
+  rm -f -r build
+  mkdir -p build
+  cd build
+  cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+  make . -j4 all
+}
+
+build_autotester() {
+  cd "${rootdir}"
+  cd Team12/Code12/
+  rm -f -r build
+  mkdir -p build
+  cd build
+  cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+  make . -j4 autotester
+}
+
+tests() {
+  echo "Running unit_tests..."
+  "${builddir}/src/unit_testing/unit_testing"
+  echo "Running integration_tests..."
+  "${builddir}/src/unit_testing/integration_testing"
+}
+
+# build fresh copy of SPA if desired
+if [ $# -eq 0 ]
+then
+  echo_usage
+  exit
+elif [ $# -eq 1 ]
+then
+  if [ $1 == "no-build" ];
+  then
+    echo "Skipping CMake build..."
+    tests
+  elif [ $1 == "build" ];
+  then
+    build_all
+    tests
+  else
+    echo_usage
+    exit
+  fi
+elif [ $# -eq 2 ]
+then
+  if [ \($1 == "build"\) -a \($2 == skip-tests\) ];
+  then
+    build_autotester
+    echo "Skipping tests..."
+  elif [ \($1 == "no-build"\) -a \($2 == skip-tests\) ];
+  then
+    echo "Skipping CMake build..."
+    echo "Skipping tests..."
+  else
+    echo_usage
+    exit
+  fi
+else
+  echo_usage
+  exit
 fi
 
-autotester="${builddir}/src/autotester/autotester"
+echo "Run autotester..."
+autotester="${builddir}/../cmake-build-debug/src/autotester/autotester"
 outdir="${rootdir}/Team12/Code12/tests/output"
-rm -rf ${outdir}
-mkdir -p ${outdir}
+rm -rf "${outdir}"
+mkdir -p "${outdir}"
 cp "${rootdir}/Team12/Code12/tests/analysis.xsl" "${outdir}/analysis.xsl"
 
 # run tests
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b") # ignore spaces in filename
 for sourcefile in ${testdir}/Sample_source*.txt; do
-  queryfile="$(echo "$sourcefile " | sed -e 's/source/queries/g' -e 's/Source/Queries/g')"
-  outfile="${outdir}/$(basename -s .txt $sourcefile | sed 's/Sample_source_/output_/g').XML"
-  $autotester $sourcefile $queryfile $outfile
+  queryfile="$(echo "$sourcefile" | sed -e 's/source/queries/g' -e 's/Source/Queries/g')"
+  testdescription=$(basename -s .txt $sourcefile | sed 's/Sample_source_/output_/g')
+  outfile="${outdir}/${testdescription}.XML"
+  echo "Running AutoTester for $testdescription..."
+  DUMMY=$("$autotester" "$sourcefile" "$queryfile" "$outfile")
 done
+# restore $IFS
+IFS=$SAVEIFS
 
+echo "Test results written to $outdir"
 firefox "file://${outdir}"
