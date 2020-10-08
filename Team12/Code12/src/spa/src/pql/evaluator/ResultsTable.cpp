7d20a07344f134abb29ab72cb8fee82764ae4e71
Alden <49450743+alcen@users.noreply.github.com>
2020-10-09 02:10:23 +0800

    Implement operator== for ResultsRelation

--- Team12/Code12/src/spa/src/pql/evaluator/ResultsTable.cpp
+++ Team12/Code12/src/spa/src/pql/evaluator/ResultsTable.cpp
@@ -131,7 +131,12 @@ class ResultsRelation {
 public:
     String value1;
     String value2;
+
     ResultsRelation(String v1, String v2): value1(std::move(v1)), value2(std::move(v2)) {}
+    bool operator==(const ResultsRelation& rr) const
+    {
+        return this->value1 == rr.value1 && this->value2 == rr.value2;
+    }
 };
 
 // Hash function for ResultsRelation
