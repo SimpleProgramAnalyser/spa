bdb72a8af39ed18f45226c7758788a5244f195e7
Alden <49450743+alcen@users.noreply.github.com>
2020-10-15 19:17:11 +0800

    Rename WithQualifier -> WithUnifier

---
+++ Team12/Code12/src/spa/src/pql/evaluator/attribute/WithUnifier.cpp
@@ -0,0 +1,10 @@
+/**
+ * Implementation of WithUnifier for Query Evaluator.
+ * WithUnifier performs a unification to obtain results.x`
+ */
+
+#include "WithUnifier.h"
+
+#include "pkb/PKB.h"
+
+Void evaluateWith(WithClause* whClause, ResultsTable* resultsTable) {}
