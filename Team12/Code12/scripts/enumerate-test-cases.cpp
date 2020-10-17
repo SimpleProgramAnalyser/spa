26941336381bf069c08c1c5524170973977ddb3e
Alden <49450743+alcen@users.noreply.github.com>
2020-10-17 23:09:05 +0800

    Fix numbering for system tests (queries)
    * New script to number queries
    
    * Use TRUE instead of true

---
+++ Team12/Code12/scripts/enumerate-test-cases.cpp
@@ -0,0 +1,59 @@
+/**
+ * A program to enumerate AutoTester test queries properly.
+ */
+
+#include <fstream>
+#include <iostream>
+#include <regex>
+
+int main(int argc, char* argv[])
+{
+    std::regex commentSyntax("(\\d+[a-z]*)(\\ *)(- )");
+
+    if (argc < 2) {
+        std::cout << "Usage: ./enumerate-test-cases <filename>" << std::endl;
+        return 1;
+    }
+
+    std::ifstream testQueries;
+    testQueries.open(argv[1]);
+    if (testQueries.fail() || testQueries.bad()) {
+        std::cerr << "Error opening file. Terminating." << std::endl;
+        return 1;
+    }
+
+    std::stringstream output;
+    std::string line;
+    std::int32_t index = 1;
+    while (std::getline(testQueries, line)) {
+        std::string currentQueryNumber = std::to_string(index) + " - ";
+        std::string newLine;
+
+        try {
+            newLine = std::regex_replace(line, commentSyntax, currentQueryNumber);
+        } catch (const std::regex_error& e) {
+            std::cerr << "Regex error. Terminating." << std::endl;
+            testQueries.close();
+            return 1;
+        }
+        if (std::regex_search(line, commentSyntax)) {
+            index++;
+        }
+        output << newLine << '\n';
+    }
+
+    std::ofstream newTestQueries;
+    newTestQueries.open(argv[1]);
+    newTestQueries << output.rdbuf();
+    if (newTestQueries.fail() || newTestQueries.bad()) {
+        std::cerr << "Error writing to file. Terminating." << std::endl;
+        testQueries.close();
+        newTestQueries.close();
+        return 1;
+    }
+
+    std::cout << "Enumeration successful. File saved." << std::endl;
+    testQueries.close();
+    newTestQueries.close();
+    return 0;
+}
