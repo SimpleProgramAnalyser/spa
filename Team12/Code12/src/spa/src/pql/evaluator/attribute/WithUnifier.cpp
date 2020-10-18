10f506d2ca00c2854e1d6d33db340acd3d96cdb8
Alden <49450743+alcen@users.noreply.github.com>
2020-10-18 22:24:22 +0800

    Fix only one result matching for With Clause

--- Team12/Code12/src/spa/src/pql/evaluator/attribute/WithUnifier.cpp
+++ Team12/Code12/src/spa/src/pql/evaluator/attribute/WithUnifier.cpp
@@ -215,7 +215,6 @@ Void unifyLeftKnown(const Reference& literalRef, const Reference& varRef, Result
         // try to find a substitution that matches
         if (comparisonFunction(pair.attributeResult, rawLiteral)) {
             matchingResults.push_back(pair.synonymResult);
-            break;
         }
     }
     resultsTable->storeResultsOne(varRef.getValue(), matchingResults);
