9c0a6d03d25cdec9630954b6cb57919b5bcfc9d7
Tan Yee Jian <tanyeejian@gmail.com>
2020-10-27 22:08:53 +0800

    some work

--- Team12/Code12/src/spa/src/pql/optimiser/Optimiser.cpp
+++ Team12/Code12/src/spa/src/pql/optimiser/Optimiser.cpp
@@ -6,8 +6,11 @@
 
 Void optimiseQuery(AbstractQuery& abstractQuery)
 {
+    // preprocessing
     substituteWithValues(abstractQuery);
     deleteDuplicateClauses(abstractQuery);
+
+    // group and sort
     groupQueryClauses(abstractQuery);
     sortWithinEachClause(abstractQuery);
     sortClauses(abstractQuery);
@@ -16,5 +19,21 @@ Void optimiseQuery(AbstractQuery& abstractQuery)
 Void substituteWithValues(AbstractQuery& abstractQuery) {}
 Void deleteDuplicateClauses(AbstractQuery& abstractQuery) {}
 Void groupQueryClauses(AbstractQuery& abstractQuery) {}
+
+/**
+ * 1. Least synonyms (1) first
+ * 2. Least result (Modifies, Follows) first
+ * 3. Prevent both cold synonyms
+ * 4. With before such that
+ *
+ * @param abstractQuery
+ * @return
+ */
 Void sortWithinEachClause(AbstractQuery& abstractQuery) {}
+
+/**
+ *
+ * @param abstractQuery
+ * @return
+ */
 Void sortClauses(AbstractQuery& abstractQuery) {}
