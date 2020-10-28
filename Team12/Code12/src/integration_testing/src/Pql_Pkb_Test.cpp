1c1a4aadc80e7fd35cfcb58efeee55c27ff46b2b
Alden <49450743+alcen@users.noreply.github.com>
2020-10-28 21:00:12 +0800

    Add integration tests for same attribute names

--- Team12/Code12/src/integration_testing/src/Pql_Pkb_Test.cpp
+++ Team12/Code12/src/integration_testing/src/Pql_Pkb_Test.cpp
@@ -3229,3 +3229,96 @@ TEST_CASE("query with such that Uses clause, left operand synonym, right operand
     // REQUIRE(formattedQueryResult.getResults() == expectedResultsStr);
     REQUIRE(formattedQueryResult == expectedFormattedQueryResults);
 }
+
+TEST_CASE("Query selecting attributes should not return duplicates - Print # VariableName;;")
+{
+    resetPKB();
+    String varName = "aprendoEspanol";
+    String procName = "aprendesEspanol";
+    Vector<String> varNames1({varName});
+    Vector<String> varNames2({varName});
+    insertIntoVariableTable(varName);
+    insertIntoProcedureTable(procName, 1, 2);
+    insertIntoStatementTable(1, PrintStatement);
+    insertIntoStatementTable(2, PrintStatement);
+    addUsesRelationships(1, PrintStatement, varNames1);
+    addUsesRelationships(2, PrintStatement, varNames2);
+
+    String query = "print pn; Select pn.varName";
+    QueryResultFormatType format = AutotesterFormat;
+    UiStub ui;
+    FormattedQueryResult actual = PqlManager::executeQuery(query, format, ui);
+    FormattedQueryResult expected("aprendoEspanol");
+    REQUIRE(actual == expected);
+
+    String query2 = "print pn; variable v; Select <pn.varName, v>";
+    FormattedQueryResult actual2 = PqlManager::executeQuery(query2, format, ui);
+    FormattedQueryResult expected2("aprendoEspanol aprendesEspanol");
+    REQUIRE(actual2 == expected2);
+
+    String query3 = "print pn; variable v; procedure p; Select <pn.varName, v, p>";
+    FormattedQueryResult actual3 = PqlManager::executeQuery(query3, format, ui);
+    FormattedQueryResult expected3("aprendoEspanol aprendoEspanol aprendesEspanol");
+    REQUIRE(actual3 == expected3);
+}
+
+TEST_CASE("Query selecting attributes should not return duplicates - Read # VariableName;;")
+{
+    resetPKB();
+    String varName = "aprendamosEspanol";
+    String procName = "aprendeisEspanol";
+    Vector<String> varNames1({varName});
+    Vector<String> varNames2({varName});
+    insertIntoVariableTable(varName);
+    insertIntoProcedureTable(procName, 1, 2);
+    insertIntoStatementTable(1, ReadStatement);
+    insertIntoStatementTable(2, ReadStatement);
+    addModifiesRelationships(1, ReadStatement, varNames1);
+    addModifiesRelationships(2, ReadStatement, varNames2);
+
+    UiStub ui;
+    QueryResultFormatType format = AutotesterFormat;
+    String query1 = "read r; Select r.varName";
+    FormattedQueryResult actual1 = PqlManager::executeQuery(query1, format, ui);
+    FormattedQueryResult expected1("aprendamosEspanol");
+    REQUIRE(actual1 == expected1);
+
+    String query2 = "read r; variable v; Select <r.varName, v>";
+    FormattedQueryResult actual2 = PqlManager::executeQuery(query2, format, ui);
+    FormattedQueryResult expected2("aprendamosEspanol aprendamosEspanol");
+    REQUIRE(actual2 == expected2);
+
+    String query3 = "read r; variable v; procedure p; Select <r.varName, v, p>";
+    FormattedQueryResult actual3 = PqlManager::executeQuery(query3, format, ui);
+    FormattedQueryResult expected3("aprendamosEspanol aprendamosEspanol aprendeisEspanol");
+    REQUIRE(actual3 == expected3);
+}
+
+TEST_CASE("Query selecting attributes should not return duplicates - Call # ProcedureName;;")
+{
+    resetPKB();
+    String procName1 = "porLaMananaAprendenEspanol";
+    String procName2 = "porLaNocheAprendenEspanol";
+    String varName = "porLaTardeAprendeEspanol";
+    insertIntoVariableTable(varName);
+    insertIntoStatementTable(1, procName1);
+    insertIntoStatementTable(2, procName1);
+    insertIntoProcedureTable(procName2, 1, 2);
+
+    String query1 = "call cl; Select cl.procName";
+    QueryResultFormatType format = AutotesterFormat;
+    UiStub ui;
+    FormattedQueryResult actual1 = PqlManager::executeQuery(query1, format, ui);
+    FormattedQueryResult expected1("porLaMananaAprendenEspanol");
+    REQUIRE(actual1 == expected1);
+
+    String query2 = "call cl; variable v; Select <cl.procName, v>";
+    FormattedQueryResult actual2 = PqlManager::executeQuery(query2, format, ui);
+    FormattedQueryResult expected2("porLaMananaAprendenEspanol porLaTardeAprendeEspanol");
+    REQUIRE(actual2 == expected2);
+
+    String query3 = "call cl; variable v; procedure p; Select <cl.procName, v, p>";
+    FormattedQueryResult actual3 = PqlManager::executeQuery(query3, format, ui);
+    FormattedQueryResult expected3("porLaMananaAprendenEspanol porLaTardeAprendeEspanol porLaNocheAprendenEspanol");
+    REQUIRE(actual3 == expected3);
+}
