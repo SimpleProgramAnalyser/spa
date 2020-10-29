79da2da19efbbb82f9be251fd1e5603734b3ffb3
Alden <49450743+alcen@users.noreply.github.com>
2020-10-29 21:25:32 +0800

    Extract with clause tests into another file
    * Add new file PreprocessorTestingUtils

---
+++ Team12/Code12/src/unit_testing/src/pql/preprocessor/PreprocessorTestingUtils.h
@@ -0,0 +1,12 @@
+/**
+ * Helper methods for unit testing of preprocessor.
+ */
+
+#ifndef UNIT_TESTING_PREPROCESSOR_TESTING_UTILS_H
+#define UNIT_TESTING_PREPROCESSOR_TESTING_UTILS_H
+
+#include "pql/preprocessor/Preprocessor.h"
+
+AbstractQuery processQuery(const String& queryString);
+
+#endif // UNIT_TESTING_PREPROCESSOR_TESTING_UTILS_H
