7f7888934269b1a51a6df8eda8b83396dda08d96
Alden <49450743+alcen@users.noreply.github.com>
2020-10-29 12:22:06 +0800

    Add AffectsBipFacade for Affects BIP evaluator

---
+++ Team12/Code12/src/spa/src/pql/evaluator/relationships/affects/AffectsBipFacade.h
@@ -0,0 +1,52 @@
+/**
+ * This class contains methods for Affects Evaluator to
+ * retrieve and make use of the Control Flow Graph and
+ * Next table with branching into procedures (BIP).
+ */
+
+#ifndef SPA_PQL_AFFECTS_BIP_FACADE_H
+#define SPA_PQL_AFFECTS_BIP_FACADE_H
+
+#include "AffectsEvaluatorFacade.h"
+
+class AffectsBipFacade: public AffectsEvaluatorFacade {
+    AffectsBipFacade() = default;
+    AffectsBipFacade(const AffectsBipFacade&) = default;
+    AffectsBipFacade(AffectsBipFacade&&) = default;
+    ~AffectsBipFacade() override = default;
+    AffectsBipFacade& operator=(const AffectsBipFacade&) = default;
+    AffectsBipFacade& operator=(AffectsBipFacade&&) = default;
+
+    /**
+     * Returns a list of statement numbers that are directly
+     * after the statement number specified in the Control
+     * Flow Graph with branching into other procedures,
+     * as described by the "NextBip" relationship.
+     */
+    Vector<Integer> getNext(Integer stmtNum) override;
+
+    /**
+     * Returns a list of statement numbers that are directly
+     * before the statement number specified in the Control
+     * Flow Graph with branching into other procedures,
+     * as described by the "NextBip" relationship.
+     */
+    Vector<Integer> getPrevious(Integer stmtNum) override;
+
+    /**
+     * Returns a list of procedures that have a Control Flow
+     * Graph with branching into procedures in the program.
+     */
+    Vector<String> getRelevantProcedures() override;
+
+    /**
+     * Returns the root node of the Control Flow Graph
+     * with branching into procedures (CFG BIP) of the
+     * procedure with the name provided. If the
+     * procedure does not have a CFG BIP, this
+     * will return nullptr.
+     */
+    CfgNode* getCfg(const String& procedureName) override;
+};
+
+#endif // SPA_PQL_AFFECTS_BIP_FACADE_H
