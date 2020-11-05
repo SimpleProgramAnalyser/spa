9ac55c4ebed9a857bd15da02331f6e0e49d09502
Alden <49450743+alcen@users.noreply.github.com>
2020-11-05 11:22:33 +0800

    Fix one more uint

--- Team12/Code12/src/spa/src/pql/optimiser/GroupedClauses.cpp
+++ Team12/Code12/src/spa/src/pql/optimiser/GroupedClauses.cpp
@@ -346,7 +346,7 @@ const Vector<Integer>& GroupedClauses::getGroup(int groupIndex) const
     return listOfGroups[groupIndex];
 }
 
-void GroupedClauses::applyArrangementToGroup(std::queue<uint> arrangement, int groupIndex)
+void GroupedClauses::applyArrangementToGroup(std::queue<unsigned int> arrangement, int groupIndex)
 {
     Vector<int> newGroup;
     while (!arrangement.empty()) {
